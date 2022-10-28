e404675b54ac73a255ae697d9fc8001571875b84
Osman Mamedov <osmanmuhammedoglu@gmail.com>
2022-10-28 19:17:49 +0300

    first commit

---
+++ Middleware/Controllers/EncryptedNumbersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Controllers
+{
+    [ApiController]
+    [Route("/api/EncryptedNumbers")]
+    public class EncryptedNumbersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (HttpContext.Items["numberDecrypt"] != null)
+            {
+                return Ok((Convert.ToInt32(HttpContext.Items["numberDecrypt"]) * 5).ToString());
+            }
+            return BadRequest();
+        }
+    }
+}
